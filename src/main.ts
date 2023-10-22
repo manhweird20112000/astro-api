fe2a5793d0878f3c5cd0ceb9337d074319d89245
weird <dmanh20112000@gmail.com>
2023-10-22 19:57:10 +0700

    :sparkles: add document

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { initializeTransactionalContext } from 'typeorm-transactional';
 import { I18nValidationExceptionFilter, I18nValidationPipe } from 'nestjs-i18n';
 import { HttpStatus } from '@nestjs/common';
 import { Helper } from '@/utils/helper';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -17,6 +18,14 @@ async function bootstrap() {
       errorHttpStatusCode: HttpStatus.BAD_REQUEST,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
